f411272fa39dd57a523a7b7ec5c1e100d992d046
tariqul420 <tariqul.islam.programmer@gmail.com>
2024-08-19 20:47:45 +0600

    navbar hide and show

--- app.js
+++ app.js
@@ -50,3 +50,13 @@ themeCheck();
 
 // Website First Visit on Dark Mode
 if (sunIcon === ".sun") themeSwitch();
+
+// Nav Bar Hide And Show
+function showSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+}
+function hideSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+}
